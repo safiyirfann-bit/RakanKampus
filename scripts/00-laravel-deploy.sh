186ae04958e2e4d5b790a8bd235ad90447a201a3
safiyirfann-bit <safiyirfann-bit@users.noreply.github.com>
2026-08-29 05:11:35 +0800

    Temporarily disable migration for first deploy test

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -8,5 +8,5 @@ php artisan config:cache
 echo "Caching routes..."
 php artisan route:cache
 
-echo "Running migrations..."
-php artisan migrate --force
+# echo "Running migrations..."
+# php artisan migrate --force
